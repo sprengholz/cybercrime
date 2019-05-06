28ff3a671171cf99fdcc92c8e61a7bad3b394755
Philipp Sprengholz <philipp.sprengholz@uni-jena.de>
2019-05-06 23:03:30 +0200

    Added SVG

--- js/plugins/jspsych-flyin.js
+++ js/plugins/jspsych-flyin.js
@@ -40,9 +40,23 @@ jsPsych.plugins["flyin"] = (function () {
             complete: function(anim){
                 
                 if (abortion == trial.abortionExpected) {
-                    $('.silhouetteFixed').css('background-image', "url('img/positive.jpg')");
+                    //$('.silhouetteFixed').css('background-image', "url('img/positive.jpg')");
+
+                    anime({
+                        targets: "#mouth",
+                        duration: 1000,
+                        d: "M17 50 Q 25 50 33 50", 
+                        autoplay: true
+                    });
                 } else {
-                    $('.silhouetteFixed').css('background-image', "url('img/negative.jpg')");
+                    //$('.silhouetteFixed').css('background-image', "url('img/negative.jpg')");
+
+                    anime({
+                        targets: "#mouth",
+                        duration: 1000,
+                        d: "M17 51 Q 25 48 33 49", 
+                        autoplay: true
+                    });
                 }
                 
                 answerArrived = true;
@@ -112,7 +126,7 @@ jsPsych.plugins["flyin"] = (function () {
 
 
         // show stimulus word by word
-        display_element.innerHTML = ' <div class="silhouetteFixed" style="background-image: url(\'img/neutral.jpg\');">  </div>   <div class="flyin">' + trial.answer + '</div>';
+        display_element.innerHTML = ' <div class="silhouetteFixed"> <svg viewBox="0 0 50 60" xmlns="http://www.w3.org/2000/svg"><g id="hat"><polygon id="hatTop" points="6,15 42,15 46,9 24,1 2,9" fill="none" stroke="black" /><polyline id="hatMiddle" points="6,15 6,18 42,18 42,15" fill="none" stroke="black" /><path id="hatShield" d="M6,18 Q 25 30 42 18" stroke="black" fill="transparent"/><circle id="hatBadge" cx="24" cy="9" r="3" stroke="black" fill="transparent"/></g><line id="browLeft" x1="10" y1="27" x2="20" y2="29" stroke="black" /><line id="browRight" x1="28" y1="29" x2="38" y2="27" stroke="black" /><ellipse id="eyeLeft" cx="15" cy="34" rx="2" ry="2" /><ellipse id="eyeRight" cx="33" cy="34" rx="2" ry="2" /><path id="mouth" d="M17 50 Q 25 50 33 50" stroke="black" fill="transparent"/></svg> </div>   <div class="flyin">' + trial.answer + '</div>';
         
         console.log("ABORTION EXPECTED: " + trial.abortionExpected);
         
